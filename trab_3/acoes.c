58a3202836a5a7259efcec7062ee647127011089
Talles Siqueira Ceolin <tsceolin@inf.ufsm.br>
2019-06-22 02:57:31 -0300

    Add files via upload

--- trab_3/acoes.c
+++ trab_3/acoes.c
@@ -4,22 +4,24 @@
 #define COLOR_RED     "\x1b[31m"
 #define COLOR_GREEN   "\x1b[32m"
 #define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
 #define COLOR_B_CYAN  "\x1b[1;36m"
 #define COLOR_B_RED   "\x1b[1;31m"
 
 // tempo em segundos entre os printfs
-#define SLEEP_TIME 1
+#define SLEEP_TIME 1    
 
 // bolsista entra no DCE
 void entraBolsista(){
     sleep(SLEEP_TIME);
-    printf(COLOR_B_RED "Bolsista: " COLOR_GREEN "Voltei. Vou trabalhar...\n");
+    printf(COLOR_B_RED "Bolsista: " COLOR_MAGENTA "Voltei. Vou trabalhar...\n");
 }
 
 // bolsista termina o trabalho e sai do DCE
 void saiBolsista(){
-    sleep(SLEEP_TIME);
-    printf(COLOR_B_RED "Bolsista: " COLOR_GREEN "Terminei. Fechei a porta. Vou tomar um cafézinho e já volto...\n");
+    sleep(SLEEP_TIME + 3);
+    printf(COLOR_B_RED "Bolsista: " COLOR_MAGENTA "Terminei. Fechei a porta. Vou tomar um cafézinho e já volto...\n");
 }
 
 
@@ -38,8 +40,8 @@ void entregaCarteirinha(int ficha){
 // nova ficha entra no DCE
 void entraFicha(int ficha){
     sleep(SLEEP_TIME);
-    printf(COLOR_B_CYAN "Ficha  %d: " COLOR_YELLOW "Entrei. Trouxe meus documentos.\n", ficha);
-    printf(COLOR_B_CYAN "Ficha  %d: " COLOR_YELLOW "Vou esperar pela minha carteirinha...\n", ficha);
+    printf(COLOR_B_CYAN "Ficha  %d: " COLOR_BLUE "Entrei. Trouxe meus documentos.\n", ficha);
+    printf(COLOR_B_CYAN "Ficha  %d: " COLOR_BLUE "Vou esperar pela minha carteirinha...\n", ficha);
 }
 
 // aluno recebe carteirinha
--- trab_3/carteirinhas.c
+++ trab_3/carteirinhas.c
@@ -20,63 +20,78 @@
 // estrutura de argumento das threads
 typedef struct ThreadArg {
 	int id; // número da ficha
-    sem_t sem_fichas; // semáforo de controle das fichas
+    sem_t sem_ficha; // semáforo de controle das fichas
 } threadArg;
 
 int numFichas; // número de fichas (passado pelo terminal)
-int contadorFichas; // contador para trocar as fichas
+int contadorFichas; // contador de quantas pessoas estão dentro do DCE
+int fichasDentro[LOTACAO_MAX]; // vetor com os números das fichas que estão dentro do DCE
 sem_t sem_dce;   // semáforo para esperar a sala encher 
-threadArg thread_args[LOTACAO_MAX]; // vetor de semáforo para controlar cada ficha
+sem_t sem_mutex; // semáforo para não usar o contadorFichas sem interferências  
+threadArg* thread_args; // vetor de semáforo para controlar cada ficha
 
-// troca o número das fichas
-void trocaFichas(){
-    for (int i = 0; i < LOTACAO_MAX; i++){ 
-        contadorFichas++;
-        if (contadorFichas >= numFichas)
-            contadorFichas = 1;
-        thread_args[i].id = contadorFichas;
-    }    
+// função para ver se um valor está dentro do array
+bool arrayContainValue(int val, int array[]){    
+    for(int i = 0; i < LOTACAO_MAX; i++){
+        if(array[i] == val)
+            return true;
+    }
+    return  false;
 }
 
+// função da thread do bolsista
 void* bolsista (void* arg) {
-    
+
     while (true){
+        
         sem_wait(&sem_dce); // espera entrar 5 fichas
         entraBolsista(); // bolsista entra, fecha a porta, ninguém mais entra
-        for (int i = 0; i < LOTACAO_MAX; i++){ // faz a carteirinhna das 5 fichas presentes
-            verificaDocumentos(thread_args[i].id);
-            entregaCarteirinha(thread_args[i].id);
-            sem_post(&(thread_args[i].sem_fichas)); // libera ficha para receber carteirinha
+
+        for (int i = 0; i < LOTACAO_MAX; i++){
+            int index = fichasDentro[i];                 // verifica quais números de ficha estão dentro do DCE
+            verificaDocumentos(thread_args[index].id);
+            entregaCarteirinha(thread_args[index].id);
+            sem_post(&(thread_args[index].sem_ficha));   // libera ficha para receber carteirinha
         }
-        saiBolsista(); // bolsista encerra
-        trocaFichas();        
-        for (int i = 0; i < LOTACAO_MAX; i++) // libera a entrada de novos alunos 
-            sem_post(&(thread_args[i].sem_fichas));            
+        
+        saiBolsista(); // Bolsista sai e número de pessoa dentro do DCE zera
+        contadorFichas = 0;
+
+        // for (int i = 0; i < numFichas; i++){
+        //     if (arrayContainValue(thread_args[i].id,fichasDentro))  // verifica se não é uma das fichas já liberadas
+        //         continue;
+        //     sem_post(&(thread_args[i].sem_ficha));   // libera próximas fichas para entrarem
+        // }        
     }
     
 	return 0;
 }
 
+// função das threads das fichas(alunos)
 void* aluno (void* arg) {
     // Recebe estrutura de argumentos
     threadArg* args = arg;
     int id = args->id;
-    int index;
 
     while (true){
-        entraFicha(id);
-        sem_post(&sem_dce); // avisa que entrou mais uma ficha
-        for (int i = 0; i < LOTACAO_MAX; i++){
-            if (thread_args[i].id == id){
-               sem_wait(&(thread_args[i].sem_fichas)); // espera bolsista liberar a carteirinha
-               index = i;
-               recebeCarteirinha(id);
+        sem_wait(&sem_mutex);  // semáforo para controlar o contador de pessoas dentro
+        if (contadorFichas < LOTACAO_MAX){ // verifica se DCE já não está lotado
+            entraFicha(id);                        
+            fichasDentro[contadorFichas] = id;    // adiciona no vetor a ficha que entrou 
+            contadorFichas++;                      
+            if (contadorFichas < LOTACAO_MAX){
+                sem_post(&sem_mutex); 
+            }else if (contadorFichas == LOTACAO_MAX){
+                sem_post(&sem_dce); // avisa que DCE está cheio
+                sem_post(&sem_mutex);    
             }
+
+            sem_wait(&(args->sem_ficha));
+            recebeCarteirinha(id);
+        }else{
+            sem_post(&sem_mutex);  
         }
-        sem_wait(&(thread_args[index].sem_fichas)); // espera bolsista fazer todas carteirinhas para sair
-        id = thread_args[index].id;
     }   
-    
 
 	return 0;
 }
@@ -86,29 +101,32 @@ int main(int argc, char const *argv[]){
     
      // Lê número de fichas através da linha de comando e guarda em variável global
     sscanf (argv[1],"%d",&numFichas);
-    contadorFichas = 1; // inicializa contador
+    contadorFichas = 0; // inicializa contador
+
+    thread_args = (threadArg*) malloc(numFichas * sizeof(threadArg));
 
     // Inicia os semáforos
-    sem_init(&sem_dce, 0, LOTACAO_MAX);
-    for (int i = 0; i < LOTACAO_MAX; i++){
-        thread_args[i].id = i + 1;
-        contadorFichas++;
-        sem_init(&(thread_args[i].sem_fichas), 0, 0);
+    sem_init(&sem_dce, 0, 0);
+    sem_init(&sem_mutex, 0, 1);
+    for (int i = 0; i < numFichas; i++){
+        thread_args[i].id = i;
+        sem_init(&(thread_args[i].sem_ficha), 0, 0);
     }
    
+
     // Cria thread do bolsista
     pthread_t bolsista_thread;
     pthread_create(&bolsista_thread, NULL, bolsista, NULL);
 
     // Cria vetor de threads (fichas) e de argumentos
-	pthread_t* fichas_threads = (pthread_t*) malloc(LOTACAO_MAX * sizeof(pthread_t));
-	for (int i = 0; i < LOTACAO_MAX; i++){
+	pthread_t* fichas_threads = (pthread_t*) malloc(numFichas * sizeof(pthread_t));
+	for (int i = 0; i < numFichas; i++){
 		pthread_create(&fichas_threads[i], NULL, aluno, &thread_args[i]);
     }
 	
     // Join das threads
     pthread_join(bolsista_thread, NULL);
-    for (int i = 0; i < LOTACAO_MAX; i++)		
+    for (int i = 0; i < numFichas; i++)		
 		pthread_join(fichas_threads[i], NULL);
 
     return 0;
